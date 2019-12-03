f745524979e20ea822ba405fda28101718b40911
Student <example@example.com>
2019-12-02 19:51:21 -0800

    add if, else if, and else. Each having their own prompts

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function()  {
+  var age = parseInt(prompt("How old is you?!"));
+
+  if (age === 18) {
+    alert("Congrats on voting in your first election!");
+    $('#ofvotingage').show();
+  }
+  else if (age > 18) {
+    $('#ofvotingage').show();
+  }
+  else {
+    alert("I applaud your political aspirations but you're not quite there yet!");
+    $('#underage').show();
+  }
+});
